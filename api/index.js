cf09199452ed715f0c883494172799925e4682b7
mortuza001 <59469219+mortuza001@users.noreply.github.com>
2026-01-19 01:09:06 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 
-// api/index.js
+// api/index.js (or api/main.js)
 const serverless = require('serverless-http');
 const app = require('../src/app');
-const handler = serverless(app);
 
+const handler = serverless(app);
 module.exports = (req, res) => {
-  console.log('[ENTRY]', req.method, req.url);
+  console.log('[ENTRY]', req.method, req.url, {
+    xMatchedPath: req.headers['x-matched-path'],
+    xNowRoute: req.headers['x-now-route'],
+  });
   return handler(req, res);
 };
